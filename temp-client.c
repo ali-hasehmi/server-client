8e6147ba7b5cc2b25471e6752f11383204ee0461
ali-hashemi <AGHashemi.1382@gmail.com>
2023-09-11 22:44:29 +0330

    fix some warnings

---
+++ temp-client.c
@@ -0,0 +1,155 @@
+// includes for basic I/O and Other operations
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+//includes for socket programming
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+long fsize(char file_name[]){
+		FILE *fp = fopen(file_name,"r");
+		if(fp == NULL){
+				perror("fsize() - fopen");
+				return -1; 
+		}
+		fseek(fp,0L,SEEK_END);
+		long file_size = ftell(fp);
+		if(file_size == -1){
+				perror("fsize() - ftell");
+				fclose(fp);
+				return -1;
+		}
+		fclose(fp);
+		return file_size;
+}
+
+int sendall(int s, char *buf, int *len)
+{
+    int total = 0;        // how many bytes we've sent
+    int bytesleft = *len; // how many we have left to send
+    int n;
+
+    while(total < *len) {
+        n = send(s, buf+total, bytesleft, 0);
+        if (n == -1) { break; }
+		if(bytesleft != n){
+				printf("need Resending Data - expected = %d ,send = %d",bytesleft,n);
+		}
+        total += n;
+        bytesleft -= n;
+    }
+
+    *len = total; // return number actually sent here
+
+    return n==-1?-1:0; // return -1 on failure, 0 on success
+} 
+
+int GetString(char *buff,size_t size){
+		int index = 0;
+		char input;
+		while(((input = getchar()) != '\n') && input != '\r'){
+				buff[index++]= input;
+				if(index+1 == size){
+						buff[index] = '\0';
+						return 1;
+				}
+						
+		}
+		buff[index] = '\0';
+		return 0;
+}
+int main(int argc, char *argv[]) {
+		// get ip and port from user
+		char ip_addr[15];
+		char port_num[7];
+		printf("IP Address: ");
+		scanf("%s",ip_addr);
+		printf("Port: ");
+		scanf("%s",port_num);
+		
+		int status,
+			client_socket;
+
+		struct addrinfo hints,
+						*res = NULL, // will point to the linked-list
+						*next;
+
+		memset(&hints,0,sizeof(hints)); // make sure the struct is empty
+		hints.ai_family = AF_UNSPEC; // don't care IPv4 or IPv6
+		hints.ai_socktype = SOCK_STREAM; // TCP stream socket
+	
+		if( (status = getaddrinfo(ip_addr, port_num, &hints,&res)) != 0 ) {
+				fprintf(stderr,"getaddrinfo error: %s\n",gai_strerror(status));
+				exit(1);
+		}	
+		// res now points to a linked-list of 1 more struct addrinfo
+		// don't forget to free up the linked-list
+		client_socket = -1;
+		status = -1;
+		for( next = res; next != NULL ; next = next->ai_next ) {
+				if((client_socket = socket( next->ai_family, next->ai_socktype,next->ai_protocol)) == -1){
+						perror("socket");
+				}
+				else{
+					if((status = connect(client_socket, next->ai_addr, next->ai_addrlen)) == -1){
+							perror("connect");
+					}
+					else{
+					break;
+					}
+				}
+		}
+		if(client_socket == -1 || status == -1) {
+				fprintf(stderr,"NO Luck Today\n");
+				exit(1);
+		}
+		printf("connected\n");
+		char buff[1024];
+		int recieved_bytes;
+		recieved_bytes = recv(client_socket, buff, 1024, 0);
+		buff[recieved_bytes] = '\0';
+		printf("%s\n",buff);
+
+		char file_path[30];
+		printf("Path To the File: ");
+		scanf("%s",file_path);
+		//GetString(file_path,30);
+		long file_size = fsize(file_path);
+		char msg[100];
+		sprintf(msg,"transfer-file --name %s --size %ld",file_path,file_size);
+		printf("file_path = %s\nfile_size=%ld\n",file_path,file_size);
+		send(client_socket,msg,strlen(msg),0);
+		long remaining = file_size;
+		FILE *fp = fopen(file_path,"r");
+		if(fp==NULL){
+				perror("fopen"); 
+				exit(1);
+		}
+		int total_send=0;
+		while(remaining){
+			int sending_size;
+			if(remaining >= 1024){
+					sending_size = 1024;
+					remaining -= 1024;
+			}
+			else{
+					sending_size = remaining;
+					remaining = 0; 
+			}
+			if (fread(buff,sending_size,1,fp)!=1){
+					printf("Not haveing equal sizes in fread!\n");
+			}
+			if(sendall(client_socket,buff,&sending_size)){
+					printf("Problem in sending Data!\n");
+			}
+			total_send += sending_size;
+			printf("%d bytes sent - total sent: %d\n",sending_size,total_send);
+			
+		}
+		fclose(fp);
+
+		return EXIT_SUCCESS;
+}
---
+++ temp-server.c
@@ -0,0 +1,112 @@
+// includes for basic operations
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+// includes for socket programming
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+
+int main(int argc, char *argv) {
+
+		int status,
+			sfd, // socket file descriptor
+			new_socket;
+
+		struct sockaddr_storage their_addr;
+		socklen_t addr_size = sizeof(their_addr);
+
+		struct addrinfo hints,
+						*servinfo = NULL, // will point to the result
+						*next;
+
+		memset(&hints,0,sizeof(hints)); // make sure the struct is empty
+		hints.ai_family = AF_UNSPEC; // don't care IPv4 or IPv6
+		hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
+		hints.ai_flags = AI_PASSIVE; // fill in my IP for me
+
+		if((status = getaddrinfo(NULL,"8585",&hints,&servinfo)) !=0) {
+				fprintf(stderr,"getaddrinfo error: %s\n",gai_strerror(status));
+				exit(1);
+		}
+		// servinfo now points to a linked list of 1 more struct addrinfo
+		// when you don't need servinfo anymore, must free linked list
+		for( next = servinfo ; next != NULL ; next = next->ai_next ) {
+				if((sfd = socket( next->ai_family, next->ai_socktype, next->ai_protocol)) != -1 ) {
+						if( (status = bind(sfd, next->ai_addr, next->ai_addrlen)) != -1) {
+								break;
+						}
+						else{
+								perror("bind");
+						}
+				}
+				else{
+						perror("socket");
+				}
+		}
+		if(sfd == -1){ // if all the addrinfo-s failed
+				exit(1);
+		}
+
+		freeaddrinfo(servinfo); // free the linked-list
+
+		// listen to incoming connections
+		if( (status = listen(sfd, 5)) == -1 ) {
+				perror("listen");
+		}
+		
+		if( (new_socket = accept(sfd, (struct sockaddr *)&their_addr, &addr_size)) == -1 ){
+				perror("accept");
+		} 
+		char buffer[1024] = "The only thing people are truly equal in is just death\n";
+		int len = strlen(buffer);
+		
+		if((status = send(new_socket, buffer, len, 0)) == -1){
+				perror("send");
+		}
+		len = 1024;
+		status = recv(new_socket,buffer,len,0);
+		buffer[status] = '\0';
+		char file_path[30];
+		long file_size;
+		printf("msg = %s\n",buffer);
+		sscanf(buffer,"transfer-file --name %s --size %ld",file_path,&file_size);
+		char new_file[45] = "server-";
+		strcat(new_file,file_path);
+		FILE *fp = fopen(new_file, "w");		
+		long total= file_size,
+			 expected_recv,
+			 real_recv,
+			 total_recv = 0;
+		while(total>0){
+				if(total>= 1024){
+						expected_recv= 1024;
+				}
+				else{
+						expected_recv = total;
+				}
+				if((real_recv=recv(new_socket,buffer,1024,0)) != expected_recv){
+						printf("Not having equal sizes in recv!\n");
+				}
+				total_recv += real_recv;
+				if(real_recv == 0 ){
+						printf("disconnect from client!\n");
+						break;
+				}
+				printf("%ld bytes recieved - total = %ld\n",real_recv,total_recv);
+				total -= real_recv;
+				if(fwrite(buffer,real_recv,1,fp) != 1){
+						printf("Not having equal sizes in fwrite!\n");
+				}
+		}
+		
+		fclose(fp);
+		
+		close(new_socket);
+		close(sfd);
+		return EXIT_SUCCESS;
+		}
+
